905e45b03680b91122e2423c900d789f2246fabe
Eren Soylu <ernsoylu@gmail.com>
2024-08-02 18:03:20 +0100

    IDE settings added. New Branch created.

---
+++ src/main/java/com/softncon/ACM_BasicMath/README.md
@@ -0,0 +1,6 @@
+# Basic Math functions Located in Java Math Library and Apache Commons Math Library
+## Java Math Library
+
+* Math.PI
+* Math.E
+* 
---
+++ src/main/java/com/softncon/ACM_BasicMath/basicMath.java
@@ -0,0 +1,9 @@
+package com.softncon.ACM_BasicMath;
+
+public class basicMath {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(Math.TAU);
+    }
+}
